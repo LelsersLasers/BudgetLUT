53f661d47baddf49436624a80e40a290a1f2689c
Millan Kumar <millankumar@gmail.com>
2025-03-14 15:22:03 -0400

    Simple ping implemented!

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}  -- allows "strings" to be Data.Text
+
 module Main where
 
 import qualified Configuration.Dotenv as Dotenv
 import System.Environment (lookupEnv)
+
+import Control.Monad (when, void)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+import UnliftIO (liftIO)
+import UnliftIO.Concurrent
+
+import Discord
+import Discord.Types
+import qualified Discord.Requests as R
+
+
 
 main :: IO ()
 main = do
@@ -10,8 +25,56 @@ main = do
 
   -- Get the DISCORD_TOKEN from the environment
   maybeToken <- lookupEnv "DISCORD_TOKEN"
-  case maybeToken of
-    Just token -> putStrLn $ "Token loaded: " ++ token
-    Nothing -> putStrLn "Error: DISCORD_TOKEN not found in .env file"
+  tok <- case maybeToken of
+    Just token -> return (T.pack token)
+    Nothing -> fail "DISCORD_TOKEN not found in environment"
 
-  putStrLn "Hello, :wink:!"
+  err <- runDiscord $ def { discordToken = tok
+                        --   , discordOnStart = startHandler testserverid
+                          , discordOnEnd = liftIO $ threadDelay (round (0.4 :: Double) * (10 ^ (6 :: Int))) >>  putStrLn "Ended"
+                          , discordOnEvent = eventHandler
+                          , discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn T.empty
+                          , discordGatewayIntent = def {
+                                                        -- gatewayIntentGuilds = True
+                                                      --  , gatewayIntentMembers = True
+                                                       gatewayIntentMessageContent = True
+                                                      --  , gatewayIntentMessageReactions = True
+                                                      --  , gatewayIntentMessageChanges = True
+                                                       }
+                          }
+  TIO.putStrLn err
+
+eventHandler :: Event -> DiscordHandler ()
+eventHandler event = case event of
+      MessageCreate m -> when (not (fromBot m) && isPing m) $ do
+        void $ restCall (R.CreateReaction (messageChannelId m, messageId m) "eyes")
+        threadDelay (2 * 10 ^ (6 :: Int))
+
+        -- A very simple message.
+        Right m' <- restCall (R.CreateMessage (messageChannelId m) "Pong")
+        void $ restCall (R.EditMessage (messageChannelId m, messageId m') (def {R.messageDetailedContent=messageContent m' <> "!"}))
+
+        latency <- getGatewayLatency
+        mLatency <- measureLatency
+
+        -- A more complex message. Text-to-speech, does not mention everyone nor
+        -- the user, and uses Discord native replies.
+        -- Use ":info" in ghci to explore the type
+        let opts :: R.MessageDetailedOpts
+            opts = def { R.messageDetailedContent = "Here's a more complex message, but doesn't ping @everyone!. Here's the current gateway latency: " <> (T.pack . show) ([latency, mLatency])
+                       , R.messageDetailedTTS = True
+                       , R.messageDetailedAllowedMentions = Just $
+                          def { R.mentionEveryone = False
+                              , R.mentionRepliedUser = False
+                              }
+                       , R.messageDetailedReference = Just $
+                          def { referenceMessageId = Just $ messageId m }
+                       }
+        void $ restCall (R.CreateMessageDetailed (messageChannelId m) opts)
+      _ -> return ()
+
+fromBot :: Message -> Bool
+fromBot = userIsBot . messageAuthor
+
+isPing :: Message -> Bool
+isPing = ("ping" `T.isPrefixOf`) . T.toLower . messageContent
