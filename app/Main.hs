1b76bb89fa2ced7f06326cb0ff8f9287d3ecea24
Millan Kumar <millankumar@gmail.com>
2025-03-14 19:41:04 -0400

    Deepseek idiomatic + formatting

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,28 @@
-{-# LANGUAGE OverloadedStrings #-}  -- allows "strings" to be Data.Text
+-- Allows "strings" to be Data.Text
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import qualified Configuration.Dotenv as Dotenv
-import System.Environment (lookupEnv)
-
-import Control.Monad (when, void, unless)
+import Control.Monad (unless, void)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
-
+import Discord
+import qualified Discord.Requests as R
+import Discord.Types
+import System.Environment (lookupEnv)
 import UnliftIO (liftIO)
 import UnliftIO.Concurrent
 
-import Discord
-import Discord.Types
-import qualified Discord.Requests as R
+-- Constants for help and error messages
+helpMessage :: T.Text
+helpMessage = "Use !lut help for more specifics about the !lut command"
 
-help :: T.Text
-help = "Use !lut help for more specifics about the !lut command"
+lutHelpMessage :: T.Text
+lutHelpMessage = "Use !lut <command> for more specifics about the !lut command"
 
-lutHelp :: T.Text
-lutHelp = "Use !lut <command> for more specifics about the !lut command"
-
-lutUnknown :: T.Text
-lutUnknown = "Unknown command. Use !lut help for more specifics about the !lut command"
+lutUnknownCommand :: T.Text
+lutUnknownCommand = "Unknown command. Use !lut help for more specifics about the !lut command"
 
 lutAddNoName :: T.Text
 lutAddNoName = "You need to provide a name for the lut. Use !lut add <name of lut>"
@@ -31,8 +30,7 @@ lutAddNoName = "You need to provide a name for the lut. Use !lut add <name of lu
 lutAddNoAttachment :: T.Text
 lutAddNoAttachment = "You need to provide exactly one attachment for the lut."
 
-
-
+-- Main function
 main :: IO ()
 main = do
   -- Load environment variables from .env file
@@ -44,104 +42,73 @@ main = do
     Just token -> return (T.pack token)
     Nothing -> fail "DISCORD_TOKEN not found in environment"
 
-  err <- runDiscord $ def { discordToken = tok
-                        --   , discordOnStart = startHandler testserverid
-                          , discordOnEnd = liftIO $ threadDelay (round (0.4 :: Double) * (10 ^ (6 :: Int))) >>  putStrLn "Ended"
-                          , discordOnEvent = eventHandler
-                          , discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn T.empty
-                          , discordGatewayIntent = def {
-                                                        -- gatewayIntentGuilds = True
-                                                      --  , gatewayIntentMembers = True
-                                                       gatewayIntentMessageContent = True
-                                                      --  , gatewayIntentMessageReactions = True
-                                                      --  , gatewayIntentMessageChanges = True
-                                                       }
-                          }
+  -- Run the Discord bot
+  err <-
+    runDiscord $
+      def
+        { discordToken = tok,
+          discordOnEnd = liftIO $ threadDelay (round (0.4 :: Double) * (10 ^ (6 :: Int))) >> putStrLn "\nDone!",
+          discordOnEvent = eventHandler,
+          discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn T.empty,
+          discordGatewayIntent = def {gatewayIntentMessageContent = True}
+        }
   TIO.putStrLn err
 
+-- Event handler
 eventHandler :: Event -> DiscordHandler ()
 eventHandler event = case event of
-      MessageCreate m -> unless (fromBot m) $ do
-        when (isHelp m) $ do
-          let opts :: R.MessageDetailedOpts
-              opts = def { R.messageDetailedContent = help
-                         , R.messageDetailedReference = Just $
-                            def { referenceMessageId = Just $ messageId m }
-                         }
-          void $ restCall (R.CreateMessageDetailed (messageChannelId m) opts)
-        when (isLut m) $ do
-          handleLut m
-          -- void $ restCall (R.CreateReaction (messageChannelId m, messageId m) "eyes")
-          -- threadDelay (2 * 10 ^ (6 :: Int))
+  MessageCreate m -> unless (fromBot m) $ handleMessage m
+  _ -> return ()
 
-          -- -- A very simple message.
-          -- Right m' <- restCall (R.CreateMessage (messageChannelId m) "Pong")
-          -- void $ restCall (R.EditMessage (messageChannelId m, messageId m') (def {R.messageDetailedContent=messageContent m' <> "!"}))
+-- Handle incoming messages
+handleMessage :: Message -> DiscordHandler ()
+handleMessage m
+  | isHelp m = sendHelpMessage m
+  | isLut m = handleLutCommand m
+  | otherwise = return ()
 
-          -- latency <- getGatewayLatency
-          -- mLatency <- measureLatency
+-- Send help message
+sendHelpMessage :: Message -> DiscordHandler ()
+sendHelpMessage m = sendMessage m helpMessage
 
-          -- -- A more complex message. Text-to-speech, does not mention everyone nor
-          -- -- the user, and uses Discord native replies.
-          -- -- Use ":info" in ghci to explore the type
-          -- let opts :: R.MessageDetailedOpts
-          --     opts = def { R.messageDetailedContent = "Here's a more complex message, but doesn't ping @everyone!. Here's the current gateway latency: " <> (T.pack . show) ([latency, mLatency])
-          --                , R.messageDetailedTTS = True
-          --                , R.messageDetailedAllowedMentions = Just $
-          --                   def { R.mentionEveryone = False
-          --                       , R.mentionRepliedUser = False
-          --                       }
-          --                , R.messageDetailedReference = Just $
-          --                   def { referenceMessageId = Just $ messageId m }
-          --                }
-          -- void $ restCall (R.CreateMessageDetailed (messageChannelId m) opts)
-      _ -> return ()
-
-handleLut :: Message -> DiscordHandler ()
-handleLut m = do
+-- Handle !lut commands
+handleLutCommand :: Message -> DiscordHandler ()
+handleLutCommand m = do
   let parts = tail $ T.words $ messageContent m
-  let originalM = Just $ def { referenceMessageId = Just $ messageId m }
   case parts of
-    ["help"] -> do
-      let opts :: R.MessageDetailedOpts
-          opts = def { R.messageDetailedContent = lutHelp
-                     , R.messageDetailedReference = originalM
-                     }
-      void $ restCall (R.CreateMessageDetailed (messageChannelId m) opts)
-    ["add"] -> do
-      let opts :: R.MessageDetailedOpts
-          opts = def { R.messageDetailedContent = lutAddNoName
-                     , R.messageDetailedReference = originalM
-                     }
-      void $ restCall (R.CreateMessageDetailed (messageChannelId m) opts) 
-    "add" : nameParts -> do
-      let attachments = messageAttachments m
-      case attachments of 
-        [a] -> do
-          let name = T.unwords nameParts
-          let opts :: R.MessageDetailedOpts
-              opts = def { R.messageDetailedContent = "Adding " <> attachmentUrl a <> " as " <> name
-                        , R.messageDetailedReference = originalM
-                        }
-          void $ restCall (R.CreateMessageDetailed (messageChannelId m) opts) 
-        _  -> do
-          let opts :: R.MessageDetailedOpts
-              opts = def { R.messageDetailedContent = lutAddNoAttachment
-                        , R.messageDetailedReference = originalM
-                        }
-          void $ restCall (R.CreateMessageDetailed (messageChannelId m) opts)
-    _  -> do
-      let opts :: R.MessageDetailedOpts
-          opts = def { R.messageDetailedContent = lutUnknown
-                     , R.messageDetailedReference = originalM
-                     }
-      void $ restCall (R.CreateMessageDetailed (messageChannelId m) opts)
+    ["help"] -> sendMessage m lutHelpMessage
+    ["add"] -> sendMessage m lutAddNoName
+    "add" : nameParts -> handleLutAdd m nameParts
+    _ -> sendMessage m lutUnknownCommand
 
+-- Handle !lut add command
+handleLutAdd :: Message -> [T.Text] -> DiscordHandler ()
+handleLutAdd m nameParts = do
+  let attachments = messageAttachments m
+  case attachments of
+    [a] -> do
+      let name = T.unwords nameParts
+      sendMessage m $ "Adding " <> attachmentUrl a <> " as " <> name
+    _ -> sendMessage m lutAddNoAttachment
+
+-- Helper function to send a message with a reference to the original message
+sendMessage :: Message -> T.Text -> DiscordHandler ()
+sendMessage m content = do
+  let opts =
+        def
+          { R.messageDetailedContent = content,
+            R.messageDetailedReference = Just $ def {referenceMessageId = Just $ messageId m}
+          }
+  void $ restCall (R.CreateMessageDetailed (messageChannelId m) opts)
+
+-- Check if a message is from a bot
 fromBot :: Message -> Bool
 fromBot = userIsBot . messageAuthor
 
-isLut :: Message -> Bool
-isLut = ("!lut" `T.isPrefixOf`) . T.toLower . messageContent
-
+-- Check if a message is a !help command
 isHelp :: Message -> Bool
 isHelp = ("!help" `T.isPrefixOf`) . T.toLower . messageContent
+
+-- Check if a message is a !lut command
+isLut :: Message -> Bool
+isLut = ("!lut" `T.isPrefixOf`) . T.toLower . messageContent
