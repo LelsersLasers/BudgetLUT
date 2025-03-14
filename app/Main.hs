b9afa08c6d67ea97000f5e8d651d19ffa2e61c43
Millan Kumar <millankumar@gmail.com>
2025-03-14 15:31:25 -0400

    !help command

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@ module Main where
 import qualified Configuration.Dotenv as Dotenv
 import System.Environment (lookupEnv)
 
-import Control.Monad (when, void)
+import Control.Monad (when, void, unless)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
@@ -15,6 +15,9 @@ import UnliftIO.Concurrent
 import Discord
 import Discord.Types
 import qualified Discord.Requests as R
+
+help :: T.Text
+help = "Use !lut help for more specifics about the !lut command"
 
 
 
@@ -46,35 +49,46 @@ main = do
 
 eventHandler :: Event -> DiscordHandler ()
 eventHandler event = case event of
-      MessageCreate m -> when (not (fromBot m) && isPing m) $ do
-        void $ restCall (R.CreateReaction (messageChannelId m, messageId m) "eyes")
-        threadDelay (2 * 10 ^ (6 :: Int))
+      MessageCreate m -> unless (fromBot m) $ do
+        when (isHelp m) $ do
+          let opts :: R.MessageDetailedOpts
+              opts = def { R.messageDetailedContent = help
+                         , R.messageDetailedReference = Just $
+                            def { referenceMessageId = Just $ messageId m }
+                         }
+          void $ restCall (R.CreateMessageDetailed (messageChannelId m) opts)
+        when (isLut m) $ do
+          void $ restCall (R.CreateReaction (messageChannelId m, messageId m) "eyes")
+          threadDelay (2 * 10 ^ (6 :: Int))
 
-        -- A very simple message.
-        Right m' <- restCall (R.CreateMessage (messageChannelId m) "Pong")
-        void $ restCall (R.EditMessage (messageChannelId m, messageId m') (def {R.messageDetailedContent=messageContent m' <> "!"}))
+          -- A very simple message.
+          Right m' <- restCall (R.CreateMessage (messageChannelId m) "Pong")
+          void $ restCall (R.EditMessage (messageChannelId m, messageId m') (def {R.messageDetailedContent=messageContent m' <> "!"}))
 
-        latency <- getGatewayLatency
-        mLatency <- measureLatency
+          latency <- getGatewayLatency
+          mLatency <- measureLatency
 
-        -- A more complex message. Text-to-speech, does not mention everyone nor
-        -- the user, and uses Discord native replies.
-        -- Use ":info" in ghci to explore the type
-        let opts :: R.MessageDetailedOpts
-            opts = def { R.messageDetailedContent = "Here's a more complex message, but doesn't ping @everyone!. Here's the current gateway latency: " <> (T.pack . show) ([latency, mLatency])
-                       , R.messageDetailedTTS = True
-                       , R.messageDetailedAllowedMentions = Just $
-                          def { R.mentionEveryone = False
-                              , R.mentionRepliedUser = False
-                              }
-                       , R.messageDetailedReference = Just $
-                          def { referenceMessageId = Just $ messageId m }
-                       }
-        void $ restCall (R.CreateMessageDetailed (messageChannelId m) opts)
+          -- A more complex message. Text-to-speech, does not mention everyone nor
+          -- the user, and uses Discord native replies.
+          -- Use ":info" in ghci to explore the type
+          let opts :: R.MessageDetailedOpts
+              opts = def { R.messageDetailedContent = "Here's a more complex message, but doesn't ping @everyone!. Here's the current gateway latency: " <> (T.pack . show) ([latency, mLatency])
+                         , R.messageDetailedTTS = True
+                         , R.messageDetailedAllowedMentions = Just $
+                            def { R.mentionEveryone = False
+                                , R.mentionRepliedUser = False
+                                }
+                         , R.messageDetailedReference = Just $
+                            def { referenceMessageId = Just $ messageId m }
+                         }
+          void $ restCall (R.CreateMessageDetailed (messageChannelId m) opts)
       _ -> return ()
 
 fromBot :: Message -> Bool
 fromBot = userIsBot . messageAuthor
 
-isPing :: Message -> Bool
-isPing = ("ping" `T.isPrefixOf`) . T.toLower . messageContent
+isLut :: Message -> Bool
+isLut = ("!lut" `T.isPrefixOf`) . T.toLower . messageContent
+
+isHelp :: Message -> Bool
+isHelp = ("!help" `T.isPrefixOf`) . T.toLower . messageContent
